ac5448c5e781cf845ed6489d4ddf91c7437c4985
nacho <nacho.conso@gmail.com>
2020-06-28 14:46:16 -0300

    fixed inputs data clearing after calculating

--- components/form-input/index.js
+++ components/form-input/index.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { Text, View, KeyboardAvoidingView } from "react-native";
 import { Input } from "@ui-kitten/components";
 import { styles } from "./styles";
-
+import PropTypes from "react-native";
 export default function CustomInput(props) {
   const [value, setValue] = useState("");
   const updateState = (props, value) => {
-    props.onChangeText(value);
-    setValue(value);
+    let parsedValue = parseFloat(value.replace(",", "."));
+    props.onChangeText(parsedValue);
+    setValue(props.value);
   };
   return (
     <View style={styles.container}>
@@ -18,10 +19,12 @@ export default function CustomInput(props) {
           placeholder={props.placeholder}
           placeholderTextColor="#C7C7CD"
           onChangeText={(value) => updateState(props, value)}
-          value={value}
           textStyle={styles.input}
+          value={String(props.value)}
         ></Input>
       </KeyboardAvoidingView>
     </View>
   );
 }
+
+const types = PropTypes.createElement;
